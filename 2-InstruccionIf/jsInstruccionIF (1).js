099f4768004d573f5eaf71ecf46e93173f8fa11a
FedericoSergio95 <60630320+FedericoSergio95@users.noreply.github.com>
2020-02-11 19:23:22 -0300

    1er ej if

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,14 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
+
+if (edad == 15) {
+    alert("niña bonita")
+};
+
+alert("No tienes 15");
 
 }//FIN DE LA FUNCIÓN
