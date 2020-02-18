3982942f52b154fdb212e65eaef009099cf9ecd1
FedericoSergio95 <60630320+FedericoSergio95@users.noreply.github.com>
2020-02-18 20:25:50 -0300

    6to ej

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,32 @@
 function mostrar()
 {
-
+	//Declaro variables
 	var contador=0;
 	var acumulador=0;
+	var numeroIngresado;
+	var promedio;
 
+//Pido los 5 numeros
+do {
+	numeroIngresado = prompt("Ingrese un numero");
+	numeroIngresado = parseInt(numeroIngresado);
+	while (isNaN(numeroIngresado)) {
+		numeroIngresado= prompt("Lo que ingresaste no era un numero, volve a ingresar uno");
+		numeroIngresado = parseInt(numeroIngresado);
+	}
+	//En esta parte del codigo sabemos que el numero es valor correcto
+	contador++; //contador = contador + 1;
+	acumulador = acumulador + numeroIngresado;
+	console.log("El contador es: "+contador);
+	console.log("Se encuentra acumulando : "+acumulador);
 
+} while (contador<5){
+	
+
+}
 
 document.getElementById('suma').value=acumulador;
+promedio = acumulador / contador;
 document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
